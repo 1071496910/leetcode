439a3b27cb25c612588e1b36f97136c12645eeb8
hepanchen <1071496910@qq.com>
2018-09-18 15:23:00 +0800

    add kth element

--- 215/1.go
+++ 215/1.go
@@ -13,5 +13,65 @@ You may assume k is always valid, 1 ≤ k ≤ array's length.*/
 package main
 
 func findKthLargest(nums []int, k int) int {
-	return 0
+	length := len(nums)
+	initHeap(nums)
+	for i := 0; i < k-1; i++ {
+		nums[0], nums[length-1] = nums[length-1], nums[0]
+		length = length - 1
+		shiftDown(nums, 0, length)
+		//fmt.Println(nums)
+	}
+	return nums[0]
+}
+
+func initHeap(nums []int) {
+	length := len(nums)
+	if length == 0 || length == 1 {
+		return
+	}
+
+	length = len(nums)
+	for i := (length / 2) - 1; i >= 0; i-- {
+		shiftDown(nums, i, length)
+	}
+}
+
+func maxIndexNum(nums []int, i, j int, length int) (int, int) {
+	if j >= length {
+		return i, nums[i]
+	}
+
+	if max(nums[i], nums[j]) == nums[i] {
+		return i, nums[i]
+	}
+
+	return j, nums[j]
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+
+func shiftDown(nums []int, i int, length int) {
+
+	needShift := true
+	for needShift {
+		needShift = false
+		left := 2*i + 1
+		right := 2*i + 2
+
+		if left >= length && right >= length {
+			return
+		}
+		maxChildIndex, maxChildValue := maxIndexNum(nums, left, right, length)
+
+		if nums[i] < maxChildValue {
+			nums[i], nums[maxChildIndex] = nums[maxChildIndex], nums[i]
+			needShift = true
+			i = maxChildIndex
+		}
+	}
 }
